4818f14a845f7991953d36e01ed652b9f4e02dab
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-08-28 12:51:12 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,16 @@
+//Print [3,5,6,7,8,] = 11111   
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+ 	cout<<"Enter a charecter ";
+ 	cin>>n;
+ 	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'){
+ 		cout<<"Number enterd is vowel";
+	 }
+	 else{
+	 	cout<<"Number Enter is Consonent";
+	 }
+}
+  
+
