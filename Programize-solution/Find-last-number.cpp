185f05975a764f76d668a74199a6ce116970460d
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-08-28 23:48:46 +0530

    Add files via upload

---
+++ Programize-solution/Find-last-number.cpp
@@ -0,0 +1,17 @@
+
+//Find the  last number of givven number
+// if 2345 = 5
+// if  78  = 8
+//if    7  = 7
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int num,ans;
+	cout<<"Enter a number";
+	cin>>num;
+	ans=num%10;
+	cout<<ans;
+}
