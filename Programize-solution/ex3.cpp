eb5c1e9a704e8c83c1f7b1c56e481952aea942b3
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-08-28 13:16:55 +0530

    Add files via upload

---
+++ Programize-solution/ex3.cpp
@@ -0,0 +1,17 @@
+// Program to Find Size of int, float, double and char in Your System
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int integer_size;
+	float float_size;
+	double double_size;
+	char char_size;
+	
+	cout<<"Size of integer "<<sizeof(integer_size)<<endl;
+	cout<<"Size of float "<<sizeof(float_size)<<endl;
+	cout<<"Size of double "<<sizeof(double_size)<<endl;
+	cout<<"Size of char "<<sizeof(char_size)<<endl;	
+	
+}
