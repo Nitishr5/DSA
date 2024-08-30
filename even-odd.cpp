9875f9f8d222b34eb061a7fcf553f6b4a40ca846
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-08-30 23:27:33 +0530

    Add files via upload

---
+++ even-odd.cpp
@@ -0,0 +1,69 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	int num;
+//	cout<<"Enter a number";
+//	cin>>num;
+//	
+//	if(num%2==0)
+//	
+//		cout<<"EVEN number";
+//	else
+//		cout<<"ODD number";
+//}
+//
+
+
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	int num;
+//	cout<<"Enter a number";
+//	cin>>num;
+//	
+//	if(num/2*2==num)
+//	
+//		cout<<"EVEN number";
+//	else
+//		cout<<"ODD number";
+//}
+//
+//
+//
+//
+//
+//
+//check even using binary data--------- in last of binary value 0 it is even in last 1 it is odd
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	int num;
+//	cout<<"Enter a number";
+//	cin>>num;
+//	
+//	if(num&1)  
+//	
+//		cout<<"EVEN number";
+//	else
+//		cout<<"ODD number";
+//}
+
+
+
+
+//----------------------------------- SUM two number Without Using + operator ---------------------------------------------------->
+#include<iostream>    
+using namespace std;
+
+int main(){
+	int num1=5,num2=9,sum;
+	
+	sum= num1-(-num2);
+	
+	cout<<sum;
+}
+
