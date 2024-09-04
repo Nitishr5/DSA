0f11c0d243cbbdaf1fe9128f5cf25e2b331373a4
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-09-04 22:51:06 +0530

    Add files via upload

---
+++ Programize-solution/SumofN-natural-number.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num,sum;
+	cin>>num;
+	for(int i=1;i<=num;i++){
+		sum=sum+i;
+		cout<<sum<<" ";
+			
+	}
+//	cout<<sum<<" ";
+
+}
+
+
+
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num,sum=1;
+//	cin>>num;
+//	for(int i=2;i<=num;i++){
+//		sum=sum+i;
+//		cout<<sum<<" ";		
+//	}
+//	
+//
+//}
