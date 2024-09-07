4314c3a4ff68345738543e099492a52104dac065
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-09-07 09:19:40 +0530

    Add files via upload

---
+++ Programize-solution/ex8-naturalnum-problem.cpp
@@ -0,0 +1,16 @@
+//print this serirs-------> 2 5 10 17 26 37....
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num,sum;
+	cin>>num;
+	for(int i=1;i<=num;i++){
+		sum=i*i+1;// first find square of natural number and +1
+		cout<<sum<<" ";
+			
+	}
+
+}
