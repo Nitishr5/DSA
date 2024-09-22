fec06cc7b3d628cdaf23c13ddb55c97b8027a694
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-09-22 20:26:23 +0530

    Add files via upload

---
+++ Programize-solution/Logic-building-loop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x=(1,2,3);// in this prblem in x on by one assign the value 1,2 in last assign 3 so x=3
+	while(x)
+	{
+		cout<<x;
+		x--;
+	}
+}
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int x=(1,2,3);// in this prblem in x on by one assign the value 1,2 in last assign 3 so x=3
+	while(x)
+	{
+		cout<<x;  //output are= 3,2,1
+		x--;
+	}
+}
+
