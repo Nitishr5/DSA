326048c7cbdb00889517b1b9a62fd428fcf1909b
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-09-30 16:22:53 +0530

    Add files via upload

---
+++ Dsa-challange/ARRAY/BUBBLE-Sort-Array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[100];
+	int n,i,j;
+	
+	cout<<"Enter size of array";
+	cin>>n;
+	
+	for(i=0;i<n;i++)
+	{
+		cout<<"Enter element ";
+		cin>>arr[i];
+	}
+	
+	for(i=n-2;i>=0;i--)
+{
+	bool swapped = 0;
+	for(j=0;j<=i;j++)
+	{
+		if(arr[j]>arr[j+1])
+		{
+			swapped=1;
+			swap(arr[j],arr[j+1]);
+		}
+	}
+	
+	if(swapped==0)
+	break;
+}
+
+		
+		cout<<"Sorted Value Are : = ";
+		for(int i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+}
