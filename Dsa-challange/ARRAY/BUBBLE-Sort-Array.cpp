35fcb305e7bd8475ace9443316866cf811f2cf8e
Nitish <121827913+Nitishr5@users.noreply.github.com>
2024-09-30 17:11:21 +0530

    Update BUBBLE-Sort-Array.cpp

--- Dsa-challange/ARRAY/BUBBLE-Sort-Array.cpp
+++ Dsa-challange/ARRAY/BUBBLE-Sort-Array.cpp
@@ -19,7 +19,7 @@ int main()
 	bool swapped = 0;
 	for(j=0;j<=i;j++)
 	{
-		if(arr[j]>arr[j+1])
+		if(arr[j]>arr[j+1])	//  if we want in Descending order change > to <  .
 		{
 			swapped=1;
 			swap(arr[j],arr[j+1]);
