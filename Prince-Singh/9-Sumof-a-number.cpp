e719407dd3168707a816cc51071bdac105f31a17
Nitish <121827913+Nitishr5@users.noreply.github.com>
2025-06-29 15:43:18 +0530

    Update 9-Sumof-a-number.cpp

--- Prince-Singh/9-Sumof-a-number.cpp
+++ Prince-Singh/9-Sumof-a-number.cpp
@@ -15,3 +15,30 @@ int num,sum=0;
 	cout<<"Sum= "<<sum;
 	
 }
+
+
+
+
+2nd Way to solve this Question
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    
+    cout<<"Enter a number";
+    cin>>num;
+   int rem,rev=0;
+   while(num!=0)
+   {
+       
+   rem=num%10;
+   rev=rev+rem;
+   num=num/10;
+   }
+   cout<<rev;
+  
+}
